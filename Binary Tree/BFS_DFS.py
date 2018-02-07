82857661be1b5ea6a9cf59d0ef0e3e8aa24e6d98
Phuong <phnguyen@knox.edu>
2018-02-06 23:03:21 -0600

    for practice

---
+++ Binary Tree/BFS_DFS.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def bfs(self):
+        tovisit = [self]
+        visited = []
+        while tovisit:
+            node = tovisit.pop(0)
+            if node.left:
+                tovisit.append(node.left)
+            if node.right:
+                tovisit.append(node.right)
+            visited.append(node.val)
+
+        return visited
+
+    def dfs(self):
+        tovisit = [self]
+        visited = []
+        while tovisit:
+            node = tovisit[-1]
+            print(node.val)
+            if node.left and node.left.val not in visited:
+                tovisit.append(node.left)
+            elif node.right and node.right.val not in visited:
+                tovisit.pop()
+                tovisit.append(node.right)
+            else:
+                tovisit.pop()
+            if node.val not in visited:
+                visited.append(node.val)
+
+        return visited
