14c4f97f242f319c0586c8e4dcaff99caff60653
Phuong <phnguyen@knox.edu>
2018-02-09 10:22:22 -0600

    CTCI problem, Graph, DFS, 0(n) time, 0(n) space

---
+++ CTCI Problems/routeBetweenNodes.py
@@ -0,0 +1,25 @@
+"""
+Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a
+route between two nodes.
+"""
+
+
+def route(start, goal, graph):
+    stack = [start]
+    visited = set()
+    visited.add(start)
+
+    while stack:
+        node = stack.pop()
+        if node == goal:
+            print(visited)
+            return True
+        for adjacent in graph[node]:
+            if adjacent not in visited:
+                stack.append(adjacent)
+                visited.add(adjacent)
+    return False
+
+
+g = {1: [2, 3], 2: [3, 5], 3: [1, 4], 4: [], 5: [2]}
+print(route(5, 1, g))
