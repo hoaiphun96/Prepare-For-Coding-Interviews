53f5233f282fc454623102b76549f4b7ce5b4d2e
Phuong <phnguyen@knox.edu>
2018-02-24 13:37:13 -0600

    Easy problem, O(log(n) time, O(1) space

---
+++ Binary Search/searchInsertPosition.py
@@ -0,0 +1,45 @@
+"""
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+Example 1:
+
+Input: [1,3,5,6], 0
+Output: 0
+"""
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            middle = (left+right)//2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                right = middle - 1
+        if right < 0:
+            return 0
+        if left == len(nums):
+            return len(nums)
+        if left > right:
+            return left
