0ad461237f48517504948d56b1c388e52db01d34
Phuong <phnguyen@knox.edu>
2018-02-24 13:43:42 -0600

    Easy problem, O(log(n) time, O(1) space

---
+++ Binary Tree/guessNumber.py
@@ -0,0 +1,40 @@
+"""
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example:
+n = 10, I pick 6.
+
+Return 6.
+
+"""
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1
+        right = n
+        while left <= right:
+            middle = (left + right) // 2
+            res = guess(middle)
+            if res == 0:
+                return middle
+            elif res == -1:
+                right = middle - 1
+            else:
+                left = middle + 1
