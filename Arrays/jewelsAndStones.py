35e612eb061067732ba41a3e5d4993389afa1a50
Phuong <phnguyen@knox.edu>
2018-03-02 08:58:48 -0600

    Easy problem, 0(n) time, 0(1) space

---
+++ Arrays/jewelsAndStones.py
@@ -0,0 +1,35 @@
+"""
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+"""
+
+
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        if len(J) == 0:
+            return 0
+        count = 0
+        J = set(list(J))
+        for ch in list(S):
+            if ch in J:
+                count += 1
+        return count
