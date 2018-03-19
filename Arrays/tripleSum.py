45ad1e787264e9d2a0116cb0c89ef3956b90f9b4
Phuong <phnguyen@knox.edu>
2018-03-18 22:41:57 -0500

    Firecode, O(n^2) time, O(n) space

--- Arrays/tripleSum.py
+++ Arrays/tripleSum.py
@@ -36,25 +36,22 @@ def binarysearch(arr, left, right, number):
 
 #approach 2
 def triple_sum(integer_list, target_number):
-    ret = set()
-    length = len(integer_list)
-    for i in range(length - 2):
-        for j in range(i + 1, length - 1):
-            sum_ij = integer_list[i] + integer_list[j]
-            # do binary search for number target_number - sum_ij
-            k = binarysearch(integer_list, j + 1, length - 1, target_number - sum_ij)
-            if k != -1:
-                ret.add((integer_list[i], integer_list[j], integer_list[k]))
-    return list(ret)
-
-
-def binarysearch(arr, left, right, number):
-    while left <= right:
-        middle = (left + right) // 2
-        if arr[middle] == number:
-            return middle
-        elif arr[middle] > number:
-            right = middle - 1
-        else:
-            left = middle + 1
-    return -1
+    a_set = set()
+    output = []
+    for i in range(0,len(integer_list)):
+        j = i + 1
+        k = len(integer_list) - 1
+        while(j < k):
+            sum = integer_list[i] + integer_list[j] + integer_list[k]
+            if(sum > target_number):
+                k -= 1
+            elif sum < target_number:
+                j += 1
+            elif sum == target_number:
+                temp_tuple = (integer_list[i],integer_list[j],integer_list[k])
+                if temp_tuple not in a_set:
+                    a_set.add(temp_tuple)
+                    output.append(temp_tuple)
+                j += 1
+                k -= 1
+    return output
