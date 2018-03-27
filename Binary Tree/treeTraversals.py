ad958fcad0c07ce4beb476aba7cfc411453ddc1c
Jamie Nguyen <phnguyen@knox.edu>
2018-03-27 18:27:53 -0500

    Tree traversals

--- Binary Tree/treeTraversals.py
+++ Binary Tree/treeTraversals.py
@@ -1,103 +1,103 @@
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
 
-    #Recursive BFS
-    def bfs(self):
-        tovisit = [self]
-        visited = set()
+   #Recursive BFS
+   def bfs(self):
+       tovisit = [self]
+       visited = set()
 
-        while tovisit:
-            node = tovisit.pop(0)
-            print(node.val)
+       while tovisit:
+           node = tovisit.pop(0)
+           print(node.val)
 
-            if node.left and node.left.val not in visited:
-                tovisit.append(node.left)
+           if node.left and node.left.val not in visited:
+               tovisit.append(node.left)
 
-            if node.right and node.right.val not in visited:
-                tovisit.append(node.right)
+           if node.right and node.right.val not in visited:
+               tovisit.append(node.right)
 
-            visited.add(node)
+           visited.add(node)
 
-    #Recursive DFS
-    def dfs(self):
-        tovisit = [self]
-        visited = set()
-        visited.add(self)
+   #Recursive DFS
+   def dfs(self):
+       tovisit = [self]
+       visited = set()
+       visited.add(self)
 
-        while tovisit:
-            node = tovisit.pop()
-            print(node.val)
+       while tovisit:
+           node = tovisit.pop()
+           print(node.val)
 
-            if node.left and node.left.val not in visited:
-                tovisit.append(node.left)
+           if node.left and node.left.val not in visited:
+               tovisit.append(node.left)
 
-            if node.right and node.left.val not in visited:
-                tovisit.append(node.right)
+           if node.right and node.left.val not in visited:
+               tovisit.append(node.right)
 
-            visited.add(node)
+           visited.add(node)
 
-    #Iterative Preorder
-    def preOrder(self):
-        if not self:
-            return
-        stack = [self]
+   #Iterative Preorder
+   def preOrder(self):
+       if not self:
+           return
+       stack = [self]
 
-        while stack:
-            node = stack.pop()
-            if node:
-                print(node.val)
-                stack.append(node.right)
-                stack.append(node.left)
+       while stack:
+           node = stack.pop()
+           if node:
+               print(node.val)
+               stack.append(node.right)
+               stack.append(node.left)
 
 
-    #Iterative Inorder
-    def inOrder(self):
-        if not self:
-            return
-        done = False
-        stack = []
-        curr = self
-        while not done:
-            if curr:
-                stack.append(curr)
-                curr = curr.left
-            else:
-                if not stack:
-                    done = True
-                else:
-                    curr = stack.pop()
-                    print(curr.val)
-                    curr = curr.right
+   #Iterative Inorder
+   def inOrder(self):
+       if not self:
+           return
+       done = False
+       stack = []
+       curr = self
+       while not done:
+           if curr:
+               stack.append(curr)
+               curr = curr.left
+           else:
+               if not stack:
+                   done = True
+               else:
+                   curr = stack.pop()
+                   print(curr.val)
+                   curr = curr.right
 
-    #Recursive Inorder
-    def inOrderR(self):
-        if self:
-            if self.left:
-                self.left.inOrderR()
-            print(self.val)
-            if self.right:
-                self.right.inOrderR()
+   #Recursive Inorder
+   def inOrderR(self):
+       if self:
+           if self.left:
+               self.left.inOrderR()
+           print(self.val)
+           if self.right:
+               self.right.inOrderR()
 
-    #Recursive PreOrder
-    def preOrderR(self):
-        if self:
-            print(self.val)
-            if self.left:s
-                self.left.preOrderR()
-            if self.right:
-                self.right.preOrderR()
+   #Recursive PreOrder
+   def preOrderR(self):
+       if self:
+           print(self.val)
+           if self.left:
+               self.left.preOrderR()
+           if self.right:
+               self.right.preOrderR()
 
-    #Recursive PostOrder
-    def postOrderR(self):
-        if self:
-            if self.left:
-                self.left.postOrderR()
-            if self.right:
-                self.right.postOrderR()
-            print(self.val)
+   #Recursive PostOrder
+   def postOrderR(self):
+       if self:
+           if self.left:
+               self.left.postOrderR()
+           if self.right:
+               self.right.postOrderR()
+           print(self.val)
 
 
 t8 = TreeNode(8)
